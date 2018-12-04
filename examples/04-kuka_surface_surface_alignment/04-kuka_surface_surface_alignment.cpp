ad8c56408f31b5d73105c00eadea0aada6a6f4a3
egalbally <elenagal@stanford.edu>
2018-12-04 13:27:15 -0800

    moved control closer to object surface

--- examples/04-kuka_surface_surface_alignment/04-kuka_surface_surface_alignment.cpp
+++ examples/04-kuka_surface_surface_alignment/04-kuka_surface_surface_alignment.cpp
@@ -52,7 +52,7 @@ void simulation(Sai2Model::Sai2Model* robot, Sai2Model::Sai2Model* plate, ForceS
 
 // control link and position in link
 const string link_name = "link6";
-const Eigen::Vector3d pos_in_link = Eigen::Vector3d(0.0,0.0,0.20);
+const Eigen::Vector3d pos_in_link = Eigen::Vector3d(0.0,0.0,0.15);
 const Eigen::Vector3d sensor_pos_in_link = Eigen::Vector3d(0.0,0.0,0.05);
 Eigen::Vector3d sensed_force;
 Eigen::Vector3d sensed_moment;
